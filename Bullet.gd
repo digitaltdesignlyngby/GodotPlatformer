88add62c422213c798ad1df02db9ec3bfcb7131f
ajrpTec <ajrp@tec.dk>
2023-05-12 09:37:13 +0200

    Bullets
    1.) Tilføjer ny Area2D node - der består af en sprite node og en collision2D node
    2.) Laver TilesMap til group: "MapGroup" og laver Player til group : "PlayerGroup"
    3.) Tilføjer script til Bullet med signal på collsion der opfanges af den selv og laver "queue_free" på begge objekter hvis de ikke er i group map eller player
    4.) Ændrer script på player så den kan lave instanser af Bullet, der positioneres samme sted som Player med tilføjes til parent som er Level1...

---
+++ Bullet.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var speed = 500
+#var velocity = Vector2()
+
+func _physics_process(delta):
+	#velocity.x = speed*delta
+	#velocity = move_and_slide(velocity, Vector2.UP)
+	position.x += speed*delta #transform.x * speed * delta
+	#velocity = move_and_slide(velocity, Vector2.UP)
+"""
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free()
+	queue_free()
+"""
+
+
+func _on_Bullet_body_entered(body):
+	if !body.is_in_group("PlayerGroup") && !body.is_in_group("MapGroup"):
+		body.queue_free()
+		queue_free()
+	print("OMG A HIT!")
+	pass # Replace with function body.
--- PlayerScript.gd
+++ PlayerScript.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
-var speed : int = 400 
-var jump_speed : int = 300
-var gravity : int = 200 
+var speed : int = 50 
+var jump_speed : int = 500
+var gravity : int = 600 
 var velocity = Vector2()
 var acceleration = Vector2()
+
+var directionLeft = true #Dette er kun for kunne se hvilken retning vi skal skyde 
+var bullet = preload("res://Bullet.tscn")
+var level1 = load("res://Level1.tscn")
+
 
 #This is a fuction for player movement 
 func get_input(delta):
@@ -13,14 +18,25 @@ func get_input(delta):
 	
 	#This is for the player to move right
 	if Input.is_action_pressed("ui_right") && is_on_floor():
-		velocity.x += 10
+		velocity.x += speed
+		directionLeft = false
 	#This is for the player to move left
 	if Input.is_action_pressed("ui_left") && is_on_floor():
-		velocity.x -= 10
+		velocity.x -= speed
+		directionLeft = true
 	#This is for the player to jump 
 	if Input.is_action_just_pressed("ui_up") && is_on_floor():
 		#Checks to see if player is on the ground or not
 			velocity.y -=jump_speed
+	if Input.is_action_just_pressed("ui_accept"):
+			var b = bullet.instance(PackedScene.GEN_EDIT_STATE_MAIN)
+			b.position.x = position.x
+			b.position.y = position.y
+			if directionLeft : b.speed *= -1 
+			get_parent().add_child(b)
+			#add_child(b)
+			#level1.add_child(b)
+			#velocity.y -=jump_speed
 		
 		
 	# gravity 
@@ -33,7 +49,7 @@ func get_input(delta):
 	if position.y > 400: 
 		position.y= -400
 		position.x = 200
-		print("game over")
+		#print("game over")
 	
 	pass 
 
