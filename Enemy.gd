9229375d1625dc3db2b53713276ee830ca9a7f7f
ajrpTec <ajrp@tec.dk>
2023-05-12 13:39:29 +0200

    Enemies NPC
    1.) Laver en scene der nedarver fra Player ved at oprette en ny scene - og klikke på "instance child scene" ved siden af "+" (new scene)
    2.) Derefter højreklikke på scenen og trykke "exetend script" - det nye script hedder "Enemy.gd"
    3.) I "Enemy.gd" overskrives funktionen "get_input(delta)" og istedet for at styre med knapperne - laver vi nu random tal der styre "hop" og "bevægelse"
    4.) Der tilføjes en ny sprite der placeres ovenpå den anden ... et par solbriller... så man kan se forskel på spilleren og fjenderne

---
+++ Enemy.gd
@@ -0,0 +1,10 @@
+extends "res://PlayerScript.gd"
+
+func _ready():
+	speed = 10
+
+func get_input(delta):
+	if rand_range(-1.0,40.0) < 0:						speed 		*=	-1
+	if rand_range(-1.0,80.0) < 0 && is_on_floor():		velocity.y 	-=	jump_speed
+	if is_on_floor():									velocity.x 	+=	speed
+	updateAll(delta)
--- PlayerScript.gd
+++ PlayerScript.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-var speed : int = 50 
+var speed : int = 50
 var jump_speed : int = 500
-var gravity : int = 600 
+var gravity : int = 600
 var velocity = Vector2()
 var acceleration = Vector2()
 
-var directionLeft = true #Dette er kun for kunne se hvilken retning vi skal skyde 
+var directionLeft = true #Dette er kun for kunne se hvilken retning vi skal skyde
 var bullet = preload("res://Bullet.tscn")
 var level1 = load("res://Level1.tscn")
 
@@ -15,7 +15,6 @@ var level1 = load("res://Level1.tscn")
 func get_input(delta):
 	#velocity.x = velocity.x * 0.9
 	#velocity.x = 0 
-	
 	#This is for the player to move right
 	if Input.is_action_pressed("ui_right") && is_on_floor():
 		velocity.x += speed
@@ -26,32 +25,32 @@ func get_input(delta):
 		directionLeft = true
 	#This is for the player to jump 
 	if Input.is_action_just_pressed("ui_up") && is_on_floor():
-		#Checks to see if player is on the ground or not
-			velocity.y -=jump_speed
-	if Input.is_action_just_pressed("ui_accept"):
-			var b = bullet.instance(PackedScene.GEN_EDIT_STATE_MAIN)
-			b.position.x = position.x
-			b.position.y = position.y
-			if directionLeft : b.speed *= -1 
-			get_parent().add_child(b)
+		velocity.y -=jump_speed
+	if Input.is_action_just_pressed("shoot"):
+		var b = bullet.instance(PackedScene.GEN_EDIT_STATE_INSTANCE) ## MEGET VIGTIGT instance() HVIS EXPORT!!!
+		b.position.x = position.x
+		b.position.y = position.y
+		if directionLeft : b.speed *= -1
+		get_parent().add_child(b)
 			#add_child(b)
 			#level1.add_child(b)
 			#velocity.y -=jump_speed
-		
-		
-	# gravity 
-	velocity.y += gravity * delta 
+	updateAll(delta)
+	pass
+	
+func updateAll(delta):
+		# gravity 
+	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if is_on_floor():
 		velocity.x*=0.90
 	
-	if position.y > 400: 
+	if position.y > 400:
 		position.y= -400
 		position.x = 200
 		#print("game over")
-	
-	pass 
+	pass
 
 
 #Process the physics?
