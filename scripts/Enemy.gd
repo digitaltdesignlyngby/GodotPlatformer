cec2235f5e50701ab816c1ce3637ee8c0228ce15
anders petersen <andersjuul@hotmail.com>
2023-05-13 23:19:42 +0200

    forenkling af scripts

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,9 +3,7 @@ extends "res://scripts/PlayerScript.gd"
 func _ready():
 	speed = 10
 
-func _input(event):
-	pass
-
+#overskriver get_input --- så alle inputs ikke længere virker
 func get_input(delta):
 	if rand_range(-1.0,40.0) < 0:						speed 		*=	-1
 	if rand_range(-1.0,80.0) < 0 && is_on_floor():		velocity.y 	-=	jump_speed
--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -4,101 +4,37 @@ var speed : int = 50
 var jump_speed : int = 500
 var gravity : int = 600
 var velocity = Vector2()
-var acceleration = Vector2()
+
 
 var directionLeft = true #Dette er kun for kunne se hvilken retning vi skal skyde
 var bullet = preload("res://scenes/Bullet.tscn")
 
 func _ready():
-	
 	pass
+
+#Process the physics? ---
+func _physics_process(delta):
+	get_input(delta)
 
 #This is a fuction for player movement 
 func get_input(delta):
-	#mouseInputs(delta)
-	keyInputs(delta)
 	guiInputs(delta)
 	updateAll(delta)
 	pass
-
-func _input(event):
-	if event is InputEventScreenTouch || event is InputEventScreenDrag:
-		var mx = event.position.x
-		var my = event.position.y
-		if mx > 800 && my > 250:
-			velocity.x += speed
-			directionLeft = false
-			return
-		if mx < 300 && my > 250:
-			velocity.x -= speed
-			directionLeft = true
-			return
-		if my < 250 && mx> 300 && is_on_floor():
-			velocity.y -=jump_speed
-			return
-		#i alle andre tilfælde skyd
-		if mx > 300 && mx < 800 && my > 250: 
-			#var b = bullet.instance(PackedScene.GEN_EDIT_STATE_INSTANCE) ## MEGET VIGTIGT instance() HVIS EXPORT!!!
-			var b = bullet.instance()
-			b.position.x = position.x
-			b.position.y = position.y
-			if directionLeft : b.speed *= -1
-			get_parent().add_child(b)
-		
-
-func mouseInputs(delta):
-	var mx = get_viewport().get_mouse_position()[0]
-	var my = get_viewport().get_mouse_position()[1]
-	$Label.text = "hej"	
-	if Input.is_action_just_pressed("touch"):
-		if mx > 800 && my > 250:
-			velocity.x += speed
-			directionLeft = false
-			return
-		if mx < 300 && my > 250:
-			velocity.x -= speed
-			directionLeft = true
-			return
-		if my < 250 && mx> 300 && is_on_floor():
-			velocity.y -=jump_speed
-			return
-		#i alle andre tilfælde skyd
-		if mx > 300 && mx < 800 && my > 250: 
-			#var b = bullet.instance(PackedScene.GEN_EDIT_STATE_INSTANCE) ## MEGET VIGTIGT instance() HVIS EXPORT!!!
-			var b = bullet.instance()
-			b.position.x = position.x
-			b.position.y = position.y
-			if directionLeft : b.speed *= -1
-			get_parent().add_child(b)
 		
 func guiInputs(delta):
-	if Globals.up && is_on_floor():
+	#is_action_just_pressed... betyder kun en gang 
+	if (Globals.up 		|| Input.is_action_just_pressed("ui_up"))	&& is_on_floor(): 
 		velocity.y -=jump_speed
-	if Globals.left && is_on_floor():
+	if Globals.left 	|| Input.is_action_pressed("ui_left")  		&& is_on_floor():
 		velocity.x -= speed
 		directionLeft = true
-	if Globals.right && is_on_floor():
+	if Globals.right 	|| Input.is_action_pressed("ui_right")		 && is_on_floor():
 		velocity.x += speed
 		directionLeft = false	
-	if Globals.shoot:
+	if Globals.shoot 	|| Input.is_action_just_pressed("shoot"):
 		Globals.shoot = false
 		var b = bullet.instance() ## MEGET VIGTIGT instance() HVIS EXPORT!!!
-		b.position.x = position.x
-		b.position.y = position.y
-		if directionLeft : b.speed *= -1
-		get_parent().add_child(b)
-	
-func keyInputs(delta):	
-	if Input.is_action_pressed("ui_right") && is_on_floor():
-		velocity.x += speed
-		directionLeft = false
-	if Input.is_action_pressed("ui_left") && is_on_floor():
-		velocity.x -= speed
-		directionLeft = true
-	if Input.is_action_just_pressed("ui_up") && is_on_floor():
-		velocity.y -=jump_speed
-	if Input.is_action_just_pressed("shoot"):
-		var b = bullet.instance(PackedScene.GEN_EDIT_STATE_INSTANCE) ## MEGET VIGTIGT instance() HVIS EXPORT!!!
 		b.position.x = position.x
 		b.position.y = position.y
 		if directionLeft : b.speed *= -1
@@ -120,7 +56,4 @@ func updateAll(delta):
 	pass
 
 
-#Process the physics?
-func _physics_process(delta):
-	get_input(delta)
 
